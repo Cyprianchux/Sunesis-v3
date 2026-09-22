2d29e621ea6262e8f7c102b9c20cfe590e2d1f51
cyprianchux <cyprianchux@gmail.com>
2026-09-22 17:30:24 +0100

    Created BrandPix shared file

---
+++ components/brand-pix.tsx
@@ -0,0 +1,5 @@
+import Link from "next/link";
+
+export function BrandPix({ href = "#" }: { href?: string }) {
+  return <Link href={href} className="flex items-center gap-1 text-xl font-extrabold tracking-tight text-maroon"><img src="/images/sunesisBG.png" alt="" className="h-auto w-auto rounded-xl object-cover" /></Link>;
+}
